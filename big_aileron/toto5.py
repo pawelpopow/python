369729540b84be6356ee08ed341d61882a5a39af
ppopow <pawel.popow@gmail.com>
2022-09-11 15:10:27 +0100

    six commit

---
+++ big_aileron/toto5.py
@@ -0,0 +1,8 @@
+import random
+
+numberofnumbers = int(input("Podaj ilość typowanych liczb: "))
+maxnumber = int(input("Podaj maksymalna losowa liczbe: "))
+
+if __name__ == '__main__':
+    # print("Wytypuj: ", numberofnumbers, "maksliczba: ", maxnumber)
+    print("Wytypuj %s z %s liczb: " % (numberofnumbers, maxnumber))
